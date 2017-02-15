6152083d271db0b8e5f648aaba3d02fbb820653a
Rasheed21 <rasheedrsa@yahoo.com>
2017-02-15 14:20:49 -0500

    Initial commit

---
+++ app/src/main/java/com/example/rasheedcorp/lec8_dialogs_fragments/MainActivity.java
@@ -0,0 +1,74 @@
+
+package com.example.rasheedcorp.lec8_dialogs_fragments;
+
+import android.app.DatePickerDialog;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import rasheedcorp.lec8_dialogs_fragments.R;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        LinearLayout LinearLayout = (LinearLayout)findViewById(R.id.activity_main);
+        LinearLayout childLayout = new LinearLayout(this);
+        childLayout.setOrientation(LinearLayout.VERTICAL);
+        TextView textView=new TextView(this);
+
+        for(int i=0; i<=10; i++){
+            Button button = new Button(this);
+            button.setText(" " + i);
+            button.setWidth(30);
+            button.setHeight(10);
+            button.setOnClickListener(new MyLstn());
+            childLayout.addView(button);
+
+        }
+        LinearLayout.addView(childLayout);
+
+    }
+
+    class MyLstn implements View.OnClickListener{
+
+        //safely cast view to button
+        public void onClick(View view){
+
+            //makes a new button
+            String str =((Button)view).getText().toString();
+            Toast.makeText(MainActivity.this, "You clicked" + ((Button)view).getText(), Toast.LENGTH_LONG).show();
+
+            switch (str){
+                case  "0":
+                AlertDialog.Builder alertBox=new  AlertDialog.Builder(MainActivity.this);
+                         alertBox.setTitle("Alert When Click");
+                alertBox.setMessage("Button one is clicked");
+                alertBox.show();
+                break;
+                case"1":
+                    DatePickerDialog datePickerDialog=new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+
+                            textview.setText("Selected "+year+","+(month+1));
+                        }
+                    },2017, 0, 1);
+                    datePickerDialog.show();
+
+                    }
+            }
+        }
+    }
+
---
+++ app/src/main/java/rasheedcorp/lec8_dialogs_fragments/MainActivity.java
@@ -0,0 +1,9 @@
+package rasheedcorp.lec8_dialogs_fragments;
+
+import android.app.Activity;
+
+/**
+ * Created by boris_000 on 2/15/2017.
+ */
+public class MainActivity extends Activity {
+}
